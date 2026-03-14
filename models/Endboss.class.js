78b74ae89beb861f6cc44861fe9bd10881246ff6
Enes Kurt <kurt1643@icloud.com>
2026-03-14 13:28:06 +0100

    fix endboss image paths

--- models/Endboss.class.js
+++ models/Endboss.class.js
@@ -8,37 +8,37 @@ class Endboss extends MovableObject {
     deadTime = 0;
 
     images_alert = [
-        'img/4_enemie_boss_chicken/2_alert/g5.png',
-        'img/4_enemie_boss_chicken/2_alert/g6.png',
-        'img/4_enemie_boss_chicken/2_alert/g7.png',
-        'img/4_enemie_boss_chicken/2_alert/g8.png',
-        'img/4_enemie_boss_chicken/2_alert/g9.png',
-        'img/4_enemie_boss_chicken/2_alert/g10.png',
-        'img/4_enemie_boss_chicken/2_alert/g11.png',
-        'img/4_enemie_boss_chicken/2_alert/g12.png'
+        'img/4_enemie_boss_chicken/2_alert/G5.png',
+        'img/4_enemie_boss_chicken/2_alert/G6.png',
+        'img/4_enemie_boss_chicken/2_alert/G7.png',
+        'img/4_enemie_boss_chicken/2_alert/G8.png',
+        'img/4_enemie_boss_chicken/2_alert/G9.png',
+        'img/4_enemie_boss_chicken/2_alert/G10.png',
+        'img/4_enemie_boss_chicken/2_alert/G11.png',
+        'img/4_enemie_boss_chicken/2_alert/G12.png'
     ];
 
     IMAGES_ATTACK = [
-        'img/4_enemie_boss_chicken/3_attack/g13.png',
-        'img/4_enemie_boss_chicken/3_attack/g14.png',
-        'img/4_enemie_boss_chicken/3_attack/g15.png',
-        'img/4_enemie_boss_chicken/3_attack/g16.png',
-        'img/4_enemie_boss_chicken/3_attack/g17.png',
-        'img/4_enemie_boss_chicken/3_attack/g18.png',
-        'img/4_enemie_boss_chicken/3_attack/g19.png',
-        'img/4_enemie_boss_chicken/3_attack/g20.png'
+        'img/4_enemie_boss_chicken/3_attack/G13.png',
+        'img/4_enemie_boss_chicken/3_attack/G14.png',
+        'img/4_enemie_boss_chicken/3_attack/G15.png',
+        'img/4_enemie_boss_chicken/3_attack/G16.png',
+        'img/4_enemie_boss_chicken/3_attack/G17.png',
+        'img/4_enemie_boss_chicken/3_attack/G18.png',
+        'img/4_enemie_boss_chicken/3_attack/G19.png',
+        'img/4_enemie_boss_chicken/3_attack/G20.png'
     ];
 
     IMAGES_HURT = [
-        'img/4_enemie_boss_chicken/4_hurt/g21.png',
-        'img/4_enemie_boss_chicken/4_hurt/g22.png',
-        'img/4_enemie_boss_chicken/4_hurt/g23.png'
+        'img/4_enemie_boss_chicken/4_hurt/G21.png',
+        'img/4_enemie_boss_chicken/4_hurt/G22.png',
+        'img/4_enemie_boss_chicken/4_hurt/G23.png'
     ];
 
     IMAGES_DEAD = [
-        'img/4_enemie_boss_chicken/5_dead/g24.png',
-        'img/4_enemie_boss_chicken/5_dead/g25.png',
-        'img/4_enemie_boss_chicken/5_dead/g26.png'
+        'img/4_enemie_boss_chicken/5_dead/G24.png',
+        'img/4_enemie_boss_chicken/5_dead/G25.png',
+        'img/4_enemie_boss_chicken/5_dead/G26.png'
     ];
 
     constructor(x = 1700) {
